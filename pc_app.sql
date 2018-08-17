146b43317832a0bd204031c0e6dab7b31b858008
Kenneth Anthony A. Molina <KAAMolina.o365@dswd.gov.ph>
2018-08-17 18:40:17 +0800

    as of 8-17-2018

---
+++ pc_app.sql
@@ -0,0 +1,146 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : root@localhost
+Source Server Version : 50719
+Source Host           : localhost:3306
+Source Database       : pc_app
+
+Target Server Type    : MYSQL
+Target Server Version : 50719
+File Encoding         : 65001
+
+Date: 2018-08-17 18:39:41
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `lib_categories`
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_categories`;
+CREATE TABLE `lib_categories` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `description` char(60) NOT NULL,
+  `archive` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of lib_categories
+-- ----------------------------
+INSERT INTO lib_categories VALUES ('1', 'COMMON OFFICE SUPPLIES AND MATERIALS', '0');
+
+-- ----------------------------
+-- Table structure for `lib_mode_of_procurement`
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_mode_of_procurement`;
+CREATE TABLE `lib_mode_of_procurement` (
+  `id` int(7) NOT NULL AUTO_INCREMENT,
+  `description` varchar(60) NOT NULL,
+  `archive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of lib_mode_of_procurement
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `lib_source_of_fund`
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_source_of_fund`;
+CREATE TABLE `lib_source_of_fund` (
+  `id` int(7) NOT NULL AUTO_INCREMENT,
+  `description` varchar(60) NOT NULL,
+  `archive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of lib_source_of_fund
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `lib_unit_of_measure`
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_unit_of_measure`;
+CREATE TABLE `lib_unit_of_measure` (
+  `id` int(7) NOT NULL AUTO_INCREMENT,
+  `description` char(20) NOT NULL,
+  `archive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of lib_unit_of_measure
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_ppmp_tracking`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_ppmp_tracking`;
+CREATE TABLE `tbl_ppmp_tracking` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `office_tracking_id` varchar(20) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `created_by` int(7) NOT NULL,
+  `last_modified` datetime NOT NULL,
+  `modified_by` int(7) NOT NULL,
+  `archive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of tbl_ppmp_tracking
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_ppmp_working`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_ppmp_working`;
+CREATE TABLE `tbl_ppmp_working` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `ppmp_stage` int(7) NOT NULL,
+  `office` int(7) NOT NULL,
+  `category` int(7) NOT NULL,
+  `unit_of_measure` int(7) NOT NULL,
+  `mode_of_procurement` int(7) NOT NULL,
+  `source_of_fund` int(7) NOT NULL,
+  `unit_cost` float(9,2) NOT NULL,
+  `m1_quantity` int(6) NOT NULL,
+  `m2_quantity` int(6) NOT NULL,
+  `m3_quantity` int(6) NOT NULL,
+  `m4_quantity` int(6) NOT NULL,
+  `m5_quantity` int(6) NOT NULL,
+  `m6_quantity` int(6) NOT NULL,
+  `m7_quantity` int(6) NOT NULL,
+  `m8_quantity` int(6) NOT NULL,
+  `m9_quantity` int(6) NOT NULL,
+  `m10_quantity` int(6) NOT NULL,
+  `m11_quantity` int(6) NOT NULL,
+  `m12_quantity` int(6) NOT NULL,
+  `archive` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of tbl_ppmp_working
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_supplies`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_supplies`;
+CREATE TABLE `tbl_supplies` (
+  `id` int(7) NOT NULL AUTO_INCREMENT,
+  `supply_name` varchar(60) NOT NULL,
+  `unit_of_measure` int(4) NOT NULL,
+  `category` int(5) NOT NULL,
+  `unit_cost` float(9,2) NOT NULL,
+  `archive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of tbl_supplies
+-- ----------------------------
---
+++ ppmp-working.view.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title></title>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="lib/bootstrapv4/css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="lib/css/v4/w3.css">
+    <link rel="stylesheet" href="lib/open-iconic-master/font/css/open-iconic-bootstrap.css" >
+
+	<style type="text/css">
+		.icon {
+		width: 20px;
+		height: 20;
+		}
+
+		th {
+		    height: 100px;
+		}
+
+		table {
+		    border-collapse: collapse;
+		    width: 100%;
+		}
+	</style>
+
+</head>
+<body>
+	<!-- Header -->
+	<div class="jumbotron jumbotron-fluid"  style="margin-bottom:0">
+		<div class="container">
+			<h1>PPMP</h1>      
+			<p>DSWD procurement.</p>
+		</div>
+	</div>
+
+	<!-- Navigation Bar -->
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<a class="navbar-brand" href="#">PPMP</a>
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="#"></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#"></a>
+			</li>
+		</ul>
+	</nav>
+	<br>
+	<!-- Body -->
+	<div class="container-fluid ">
+		<div class="w3-row">
+			<div class="w3-col l2 ">&nbsp</div>
+			<div class="w3-col l8 border">
+				<div class="w3-row border w3-container">
+					<br>
+					<div class="w3-row">
+						<div class="w3-col l4 w3-container">
+							<label><strong>Category:</strong></label>
+							<select name="cars" class="custom-select">
+								<option selected>COMMON ELECTRICAL SUPPLIES</option>
+								<option value="volvo">Volvo</option>
+								<option value="fiat">Fiat</option>
+								<option value="audi">Audi</option>
+							</select>
+
+						</div>
+						<div class="w3-col l8 w3-container">
+							<label><strong>Supplies:</strong></label>
+							<select name="cars" class="custom-select">
+								<option selected>BATTERY, dry cell AA, 2 pieces per blister pack | pack | 27.00</option>
+								<option value="volvo">Volvo</option>
+								<option value="fiat">Fiat</option>
+								<option value="audi">Audi</option>
+							</select>
+						</div>
+					</div>
+					<br>
+					<div class="w3-row">
+						<div class="w3-col l6 w3-container">
+							<label><strong>Mode of Procurement:</strong></label>
+							<select name="cars" class="custom-select">
+								<option selected>NEGOTIATED PROCUREMENT - AGENCY TO AGENCY</option>
+								<option value="volvo">Volvo</option>
+								<option value="fiat">Fiat</option>
+								<option value="audi">Audi</option>
+							</select>
+
+						</div>
+						<div class="w3-col l6 w3-container">
+							<label><strong>Source of Fund:</strong></label>
+							<select name="cars" class="custom-select">
+								<option selected>NHTO</option>
+								<option value="volvo">Volvo</option>
+								<option value="fiat">Fiat</option>
+								<option value="audi">Audi</option>
+							</select>
+						</div>
+					</div>
+					<br>
+					<div class="w3-row  w3-container">
+						<div class="w3-row w3-center"><strong>Schedule/Milestone of Activities</strong></div>
+						<div class="w3-row">
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text ">Jan</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Feb</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Mar</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Apr</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">May</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Jun</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Jul</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Aug</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Sep</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Oct</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Nov</span>
+								<input type="text" class="form-control">
+							</div>
+							<div class="w3-col l1 w3-center">
+								<span class="input-group-text">Dec</span>
+								<input type="text" class="form-control">
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="w3-row ">
+						<div class="w3-col l6 w3-center w3-container">
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text">Total Quantity/Size</span>
+								</div>
+								<input type="text" class="form-control">
+							</div>
+						</div>
+						<div class="w3-col l6 w3-center w3-container">
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text">Estimated Budget</span>
+								</div>
+								<input type="text" class="form-control">
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="w3-row w3-container w3-right">
+						<button class="btn btn-outline-secondary" type="button">Add</button> 
+					</div>
+					<br>
+					<br>
+				</div>
+			</div>
+			<div class="w3-col l2 ">&nbsp</div>
+		</div>	
+		
+	</div>
+	<br>
+	<br>
+
+	<!-- Body -->
+	<div class="container-fluid border " style="overflow-x:auto;">
+		<table class="table-bordered text-center">
+			<thead>
+				<tr class="" >
+					<td scope="col" class="" rowspan="2"><strong>CODE (PAP)</strong></td>
+					<td scope="col" class="" rowspan="2"><strong>CATEGORY</strong></td>
+					<td scope="col" class="" rowspan="2"><strong>PROCUREMENT PROGRAM/PROJECT</strong></td>
+					<td scope="col" class="" rowspan="2"><strong>UNIT OF MEASURE</strong></td>
+					<td scope="col" class="" rowspan="2"><strong>MODE OF PROCUREMENT</strong></td>
+					<td scope="col" class="" rowspan="2"><strong>SOURCE OF FUND</strong> (MOOE/C.O)</td>
+					<td scope="col" class="" colspan="12">
+						<strong>SCHEDULE/MILESTONE OF ACTIVITIES</strong>
+					</td>
+					<td scope="col" class="" rowspan="2"><strong>TOTAL/QUANTITY/SIZE</strong></td>
+					<td scope="col" class="" rowspan="2"><strong>UNIT COST</strong></td>
+					<td scope="col" class="" rowspan="2"><strong>ESTIMATED BUDGET</strong></td>
+				</tr>
+				<tr class="w3-small">
+					<td><strong>JAN</strong></td>
+					<td><strong>FEB</strong></td>
+					<td><strong>MAR</strong></td>
+					<td><strong>APR</strong></td>
+					<td><strong>MAY</strong></td>
+					<td><strong>JUN</strong></td>
+					<td><strong>JUL</strong></td>
+					<td><strong>AUG</strong></td>
+					<td><strong>SEP</strong></td>
+					<td><strong>OCT</strong></td>
+					<td><strong>NOV</strong></td>
+					<td><strong>DEC</strong></td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td></td>
+					<td>123</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>	
+		</table>
+	</div>
+	<br>
+	<br>
+</body>
+</html>
